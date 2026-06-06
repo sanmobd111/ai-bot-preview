5dfe698f8dd0b3e7256fe30b9b169f2a0625ff1d
sanmo-yousof <sanmoyousof@gmail.com>
2026-06-06 12:07:11 +0600

    join founder

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import BenefitsSection from "@/components/home/benefit-section/benefits-section";
 import HeroSection from "@/components/home/hero-section";
+import JoinFounderSection from "@/components/home/join-founder-section";
 import MembershipSection from "@/components/home/membership-section";
 import NetworkStatsSection from "@/components/home/network-stats-section";
 import PartnersSection from "@/components/home/partner-section";
@@ -17,6 +18,7 @@ export default function Home() {
       </Container>
       <BenefitsSection />
       <PartnersSection />
+      <JoinFounderSection />
     </>
   );
 }
